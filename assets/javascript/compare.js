50d3fa40f8f64d4cc7d576804038166defdca073
Ilona Fleischer <Ilona Fleischer>
2017-01-19 18:52:36 -0500

    testing branches

---
+++ assets/javascript/compare.js
@@ -0,0 +1,16 @@
+var dummyObject ={
+	Affection: 0,
+	Affordable: 0,
+	Allergies: 0,
+	ChildFriendly: 0,
+	Cleanliness: 0,
+	Coat: 0,
+	Fitness: 0,
+	Intelligence: 0,
+	LifeSpan: 0,
+	Noise: 0,
+	Outdoor: 0,
+	PhysicalSpace: 0,
+	Size: 0,
+	Sounds: 0
+}
