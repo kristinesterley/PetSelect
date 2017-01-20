59c0dc5c57259ea47a5d49d07a599681eea060e7
Ilona Fleischer <Ilona Fleischer>
2017-01-19 21:17:54 -0500

    compare file and test compare file

--- assets/javascript/compare.js
+++ assets/javascript/compare.js
@@ -14,3 +14,53 @@ var dummyObject ={
 	Size: 0,
 	Sounds: 0
 }
+
+var catObject ={
+	Affection: 3,
+	Affordable: 2,
+	Allergies: 4,
+	ChildFriendly: 2,
+	Cleanliness: 2,
+	Coat: 3,
+	Fitness: 1,
+	Intelligence: 1,
+	LifeSpan: 2,
+	Noise: 1,
+	Outdoor: 1,
+	PhysicalSpace: 2,
+	Size: 2,
+	Sounds: 1
+}
+
+database = firebase.database();
+var matches=[];
+
+function compare(obj, animals){
+	matches = [];
+	for(animal in animals){
+		var sum = 0;
+		//console.log("animal", animals[animal]);
+		for(prop in obj){
+			var part = 1-(Math.abs(obj[prop]-animals[animal][prop]))/4;
+			//console.log(prop+" "+obj[prop]+", "+animals[animal][prop]);
+			sum += part;
+		}
+		var percent = (sum/14)*100;
+		matches.push({animal: animal, match:percent});
+	}
+		matches.sort(function(a,b){
+		return b.match-a.match;
+		});
+	console.log(matches);
+}
+
+var query = database.ref().orderByKey();
+query.once("value").then(function(snapshot){
+	console.log("snapshot",snapshot.val());
+	compare(dummyObject, snapshot.val());
+	compare(catObject, snapshot.val());
+	for(i=0;i<matches.length;i++){
+		$("#results").append("<img src=http://placehold.it/200x200>"+matches[i].animal+matches[i].match)
+	}
+});
+
