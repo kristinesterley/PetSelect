2528a1e9d090ed4e7d043ab52ee6e3c78caf2cb3
ChloeCacola <cscorrigan@icloud.com>
2017-01-26 14:17:34 -0500

    Styled 'front' (top of) page

--- assets/javascript/results.js
+++ assets/javascript/results.js
@@ -50,7 +50,7 @@ function getResults(quizObject){
 	console.log(quizObject);
 	compare(quizObject, snapshot.val());
 	for(i=0;i<matches.length;i++){
-		var newButton = '<li><button class="btn waves-effect waves-light green lighten-1 btn-select" style="width:' + Math.round((matches[i].match/100)*buttonWidth) + 'px"' +
+		var newButton = '<li><button class="btn waves-effect waves-light deep-orange lighten-2 btn-select" style="width:' + Math.round((matches[i].match/100)*buttonWidth) + 'px"' +
 				' data-youTubeId='+ 	matches[i].youTubeId+
 				' data-searchType=' + matches[i].searchType+
 				' data-searchTerm='+ matches[i].searchTerm + 
